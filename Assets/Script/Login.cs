503fabd10e8057fd0b7f9dc8e33313f348870dd7
crannnabethadh <crannnabethadh@outlook.com>
2020-01-21 15:22:55 +0100

    Initial commit

---
+++ Assets/Script/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    // Cached references
+    public InputField emailInputField;
+    public InputField passwordInputField;
+    public Button loginButton;
+
+
+}
---
+++ Assets/Script/Register.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Register : MonoBehaviour
+{
+    // Cached references
+    public InputField emailInputField;
+    public InputField passwordInputField;
+    public InputField confirmPasswordInputField;
+    public Button registerButton;
+
+}
