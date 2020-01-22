f4497f781627141e2fb719e3184b154a3fd1db19
crannnabethadh <crannnabethadh@outlook.com>
2020-01-22 13:47:14 +0100

    Token and Login

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private const string httpserver = "";
+    private const string httpserver = "https://clickycrateswebapi.azurewebsites.net";
+
+    private string _token;
+    public string Token
+    {
+        get { return _token; }
+        set { _token = value; }
+    }
+
+    private string _playerId;
+    public string PlayerId
+    {
+        get { return _playerId; }
+        set { _playerId = value; }
+    }
+
+
 
     public string GetHttpServer()
     {
         return httpserver;
     }
+
 }
--- Assets/Script/Login.cs
+++ Assets/Script/Login.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Login : MonoBehaviour
@@ -9,6 +11,118 @@ public class Login : MonoBehaviour
     public InputField emailInputField;
     public InputField passwordInputField;
     public Button loginButton;
+    public Button logoutButton;
+    public Text messageBoardText;
+    public GameManager gameManager;
 
+    private string httpServerAddress;
 
+    private void Start()
+    {
+        httpServerAddress = gameManager.GetHttpServer();
+    }
+
+    public void OnLoginButtonClicked()
+    {
+        StartCoroutine(TryLogin());
+    }
+
+    private void GetToken()
+    {
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "/Token", "POST");
+
+        // application/x-www-form-urlencoded
+        WWWForm dataToSend = new WWWForm();
+        dataToSend.AddField("grant_type", "password");
+        dataToSend.AddField("username", emailInputField.text);
+        dataToSend.AddField("password", passwordInputField.text);
+
+        httpClient.uploadHandler = new UploadHandlerRaw(dataToSend.data);
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+
+        httpClient.SetRequestHeader("Accept", "application/json");
+
+        httpClient.SendWebRequest();
+
+        while (!httpClient.isDone)
+        {
+            Task.Delay(10);
+        }
+
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            Debug.Log(httpClient.error);
+        }
+        else
+        {
+            string jsonResponse = httpClient.downloadHandler.text;
+            AuthorizationToken authToken = JsonUtility.FromJson<AuthorizationToken>(jsonResponse);
+            gameManager.Token = authToken.access_token;
+        }
+
+        httpClient.Dispose();
+    }
+
+    private IEnumerator TryLogin()
+    {
+        // The Content-Type header will be set to application/x-www-form-urlencoded by default.
+        if (string.IsNullOrEmpty(gameManager.Token))
+        {
+            GetToken();  // Blocking
+        }
+
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "/api/Account/UserId", "GET");
+        
+        httpClient.SetRequestHeader("Authorization", "bearer " + gameManager.Token);
+        httpClient.SetRequestHeader("Accept", "application/json");
+
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return httpClient.SendWebRequest();
+
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            Debug.Log(httpClient.error);
+        }
+        else
+        {
+            //string jsonResponse = httpClient.downloadHandler.text;
+            //gameManager.PlayerId = JsonUtility.FromJson<string>(jsonResponse);
+            gameManager.PlayerId = httpClient.downloadHandler.text;
+            messageBoardText.text += "\nWelcome " + gameManager.PlayerId + ". You are logged in!";
+            loginButton.interactable = false;
+            logoutButton.interactable = true;
+        }
+
+        httpClient.Dispose();
+        
+    }
+
+    public void OnLogoutButtonClicked()
+    {
+        StartCoroutine(TryLogout());
+    }
+
+    private IEnumerator TryLogout()
+    {
+        // The Content-Type header will be set to application/x-www-form-urlencoded by default.
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "/api/Account/Logout", "POST");
+
+        httpClient.SetRequestHeader("Authorization", "bearer " + gameManager.Token);
+
+        yield return httpClient.SendWebRequest();  // Return control to LoginButtonClickedAPI since web request returns
+        
+        if (httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            Debug.Log(httpClient.error);
+        }
+        else
+        {
+            messageBoardText.text += $"\n{httpClient.responseCode} Bye bye {gameManager.PlayerId}.";
+            gameManager.Token = string.Empty;
+            gameManager.PlayerId = string.Empty;
+            loginButton.interactable = true;
+            logoutButton.interactable = false;
+        }
+    }
 }
